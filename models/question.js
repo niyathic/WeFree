fb5bb75946e03f9cdb911622f14173f5a670bbe5
niyathic <niyathi.chakrapani@hotmail.com>
2018-12-07 19:14:40 -0500

    removed unnecessary files, added moment.js component

--- models/question.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-
-const questionSchema = new mongoose.Schema({
-    questionText: String,
-    author: String,
-    answer: String
-})
-
-module.exports = mongoose.model('Question', questionSchema);
