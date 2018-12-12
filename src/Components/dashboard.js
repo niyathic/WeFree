97d12dd1001427944a9db139776be169807e50bf
niyathic <niyathi.chakrapani@hotmail.com>
2018-12-12 02:16:01 -0500

    fixed frontend bugs

--- src/Components/dashboard.js
+++ src/Components/dashboard.js
@@ -6,7 +6,11 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import '../App.css';
 import { BrowserRouter as Router, NavLink} from "react-router-dom";
 import { Navbar, NavbarBrand, NavbarNav, NavItem, NavbarToggler, Collapse } from "mdbreact";
-// import newEvent from './newEvent';
+import newEvent from './newEvent';
+import {
+    Link,
+    Route
+} from "react-router-dom"; 
 const localizer = BigCalendar.momentLocalizer(moment);
 
 export default class Dashboard extends Component {
@@ -43,9 +47,10 @@ export default class Dashboard extends Component {
         <div className="container-fluid">
           <div className="h-100 p-5">
             <h1>hey bb u free?</h1>
-            {/* <button><Link to='/newevent'>Create Event</Link></button>  */}
+            {<Link to='/newEvent'><button>Create Event</button></Link>}
           </div>
         </div>
+      </div>
 
 
       {/* calender timeee */}
@@ -65,8 +70,6 @@ export default class Dashboard extends Component {
           />
         </div>
       </div>
-      </div>
-      {/* <Route path='/newEvent' component = {newEvent}/> */}
       </Router>
     );
   }
