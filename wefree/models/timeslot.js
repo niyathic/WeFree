a6ce7808bd1dea798cdedc0d0e68dabc3fa671d1
niyathic <niyathi.chakrapani@hotmail.com>
2018-12-12 01:52:44 -0500

    removed duplicate folder

--- wefree/models/timeslot.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-const timeslotSchema = new mongoose.Schema({
-    timeslot: String,
-    usersClicked: Object
-})
-
-module.exports = mongoose.model('Timeslot', timeslotSchema);
